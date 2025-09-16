4a111684a24cf30052d8a3e6739b41a5f1ee80ba
XuMiao1029 <2023035947@qq.com>
2025-09-16 16:23:08 +0800

    script.js

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ async function loadData() {
   const res = await fetch('data.json?_t=' + Date.now());
   const data = await res.json();
   document.getElementById('title').textContent = data.title;
-  document.getElementById('qr1').src = data.qr1;
-  document.getElementById('qr2').src = data.qr2;
+
+  if (data.qr1) document.getElementById('qr1').src = data.qr1;
+  if (data.qr2) document.getElementById('qr2').src = data.qr2;
 }
 
 document.addEventListener('DOMContentLoaded', loadData);
